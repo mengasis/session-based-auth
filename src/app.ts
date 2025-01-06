7f347141bc3aa4d3fa44817402f4547504961f4f
Manuel Venegas <mengasis@outlook.com>
2025-01-05 23:51:28 -0300

    feat: added csrf validation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import csrf from 'csurf';
+
 import corsConfig from './config/corsConfig';
-import publicRoutes from './routes/publicRoutes';
 import authRoutes from './routes/authRoutes';
 import { errorHandler } from './utils/errorHandler';
 
@@ -12,9 +13,20 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+const csrfProtection = csrf({ cookie: true });
+
 app.use(cors(corsConfig));
 
-app.use('/', publicRoutes);
+app.get('/', csrfProtection);
+
+app.get('/csrf-token', (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
+
+app.get('/health', (_, res) => {
+  res.status(201).send('status OK');
+});
+
 app.use('/auth', authRoutes);
 
 app.use(errorHandler);
--- src/routes/publicRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/health', (req, res) => {
-  res.status(201).send('status OK');
-});
-
-export default router;
